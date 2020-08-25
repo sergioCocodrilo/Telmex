7affd8cc4236083320186c31dfe42e96a603748a
sergio_macbook <sergio.solano.p@google.com>
2020-08-25 12:23:56 -0500

    migracion de tablilla

---
+++ DG/processing_files/migrar_tablilla.py
@@ -0,0 +1,134 @@
+"""
+Most common commands of S12
+"""
+
+import serial
+import s12_commands as s12
+
+def connect():
+    """Connects to serial through USB"""
+    ser = serial.Serial()
+    # parameters
+    ser.port = '/dev/ttyUSB0'
+    ser.baudrate =  9600
+    ser.bytesize = serial.EIGHTBITS
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.timeout = 1 # seconds
+    try:
+        ser.open()
+    except:
+        ser.port = '/dev/ttyUSB1'
+        ser.open()
+    if ser.isOpen():
+        return ser
+    else:
+        return None
+
+def show_menu():
+    """Shows menu and reads input"""
+    choice = -1
+    while choice not in range(0,10):
+        print("""
+
+        Selecciona una instrucción:
+
+            0. Salir
+
+            1. Consulta de dispositivo
+            2. Cambio de dispositivo
+            3. Alta
+            4. Baja
+
+            5. Backup
+            6. Resumen de todos los números
+            7. Números en un módulo
+
+            8. Cambio de varos dispositivos
+
+            9. Imprimir ubicaciones de módulos
+            
+        """)
+        usr_input = ask_user_for_input("Instrucción")
+        if usr_input.isdigit():
+            choice = int(usr_input)
+    return choice
+
+def ask_user_for_input(msg):
+    return input("{:>40}: ".format(msg))
+
+def s12_listen(ser):
+    """Listens for the answer of the S12 and returns the result"""
+    output_ended = False
+    server_response = []
+    while not output_ended:
+        server_response.extend(ser.readlines())
+        for line in server_response:
+            if b"<" in line or b">" in line:
+                output_ended = True 
+    return server_response
+    
+def build_command(choice):
+    """Builds the insctruction text"""
+    # get input parameters from user
+    if choice in [1,2,3,4]:
+        dn = ask_user_for_input("Número")
+    if choice in [2,3,7]:
+        na = ask_user_for_input("Módulo")
+    if choice in [2,3]:
+        lan = ask_user_for_input("Par")
+    if choice in [8]:
+        _ = ask_user_for_input("Los datos se tomarán de cambio_de_dispositivo")
+    if choice in [9]:
+        source_file = ask_user_for_input("Archivo con la lista de módulos")
+    
+    commands = None
+    if choice == 1:    # consulta de dispositivo
+        commands = s12.display_any_subscriber(dn)
+    elif choice == 2:  # cambio de dispositivo
+        commands = s12.new_module(dn, na, lan)
+    elif choice == 3:  # alta de número
+        commands = s12.create_single_subscriber(dn, na, lan)
+    elif choice == 4:  # baja de número
+        commands = s12.remove_single_subscriber(dn)
+    elif choice == 5:  # backup
+        commands = s12.start_backup()
+    elif choice == 6:  # resumen todos nums
+        pass
+    elif choice == 7: # números en un módulo
+        commands = s12.nums_en_mod(na)
+    elif choice == 8: # cambio de varios dispositivos
+        commands = s12.change_multiple_modules()
+    elif choice == 9:
+        commands = s12.translate_sblrit(source_file)
+        
+    return commands
+
+def execute_command(ser, commands, choice):
+    """Send commands to server"""
+    for command in commands:
+        ser.write('\x1b'.encode("ascii"))
+        s12_listen(ser)
+        ser.write(b"MM\r\n")
+        s12_listen(ser)
+        ser.write(command.encode("ascii") + b"\r\n")
+        response = s12_listen(ser)
+        [print(line[:-1].decode("ascii")) for line in response]
+        """ AGREGAR UN BREAK SI LA PRIMERA INSTRUCCIÓN FALLA.
+        NO TIENE SENTIDO TRATAR DE EJECUTAR LA SIGUIENTE,
+        TAMBIÉN FALLARÁ PERO EL SISTEMA NO RESONDE."""
+
+def display_menu():
+    """Shows the functions of the S12"""
+    ser = connect()
+    while 1:
+        choice = show_menu()
+        if choice == 0:
+            break
+        commands = build_command(choice)
+        if commands:
+            execute_command(ser, commands, choice)
+    ser.close()
+
+if __name__ == "__main__":
+    display_menu()
---
+++ DG/processing_files/s12_commands.py
@@ -0,0 +1,60 @@
+"""Builds the text for making native S12 commands"""
+
+
+# built in, single functions
+
+def nums_en_mod(na):
+    return ["157:EN=H'" + na + "."]
+
+def create_single_subscriber(dn, na, lan,):
+    return ["5289:2=K'" + dn + ",6=H'" + na + ",8=" + lan + ''',106="SUBG02",46=1,45=1&K'01&K'123&1&K'00&K'123.''']
+
+def remove_single_subscriber(dn):
+    return ["5292:DN=K'" + dn + "."]
+
+def display_any_subscriber(dn):
+    return ["5297:DN=K'" + dn + "."]
+
+
+# pipe of commands
+
+def new_module(dn, na, lan):
+    commands = remove_single_subscriber(dn)
+    commands.extend(create_single_subscriber(dn, na, lan))
+    return commands
+
+import datetime
+def start_backup():
+    """Weekly system backup"""
+    date = datetime.datetime.now() # build identifier idf
+    month = str(date.month) if date.month > 9 else "0"+ str(date.month)
+    day = str(date.day) if date.day > 9 else "0"+ str(date.day)
+    idf = "AB" + month + day
+    # mount disk
+    commands.append('8339:LDEV=DKB2,VOLID="ABASTOS".')
+    # write permission
+    commands.append('8334:LDEV=4120,PROTECT=OFF.')
+    commands.append('8334:LDEV=4121,PROTECT=OFF.')
+    # backup
+    commands.append('8331:2=Y,5=1032,7=4120,8="' + idf + '",9=4121,10="' + idf + '",11,13=15.')
+    return commands
+
+def change_multiple_modules():
+    commands = []
+    with open("Input_lists/cambio_de_dispositivo", "r") as f:
+        next(f)
+        try:
+            for line in f:
+                dn, na, lan = line.split()
+                commands.extend(new_module(dn, na, lan))
+        except:
+            raise ValueError("Error en el archivo cambio_de_dispositivo")
+    return commands
+
+def translate_sblrit(source_file):
+    commands = []
+    with open("Input_lists/" + source_file, 'r') as f:
+        for l in f:
+            commands.append('39:9=1,4=CTLE,NA=' + l + ',NBR=1.')
+    return commands
+   
